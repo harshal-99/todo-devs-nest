9bc257178a78ded6afce47e4a68827beeac27a96
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-12-09 18:49:57 +0530

    Implement reset route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"
 
 import todoRouter from "./api/todo.js";
+import {resetTodo} from "./data.js";
 
 const app = express()
 
@@ -8,6 +9,10 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use('/api/todo', todoRouter);
+app.use('/api/reset', (req, res) => {
+    resetTodo()
+    res.status(200).json({message: "Todo reset"})
+})
 
 const PORT  = process.env.PORT || 3000
 
